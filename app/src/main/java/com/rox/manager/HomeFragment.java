1acb822efd88059e0b8aec3a76eee308748dd3fb
Bi <133272416+bimuafaq@users.noreply.github.com>
2026-03-19 10:51:52 +0000

    chore: remove unused imports and delete SECURITY.md

--- app/src/main/java/com/rox/manager/HomeFragment.java
+++ app/src/main/java/com/rox/manager/HomeFragment.java
@@ -9,7 +9,6 @@ import android.widget.TextView;
 import com.google.android.material.snackbar.Snackbar;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import com.google.android.material.button.MaterialButton;
 
--- app/src/main/java/com/rox/manager/MainActivity.java
+++ app/src/main/java/com/rox/manager/MainActivity.java
@@ -1,7 +1,6 @@
 package com.rox.manager;
 
 import android.os.Bundle;
-import android.view.View;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.WindowCompat;
