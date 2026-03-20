20e319e11b95711ef61e590e95135587aad796d4
Bi <133272416+bimuafaq@users.noreply.github.com>
2026-03-20 04:23:49 +0000

    fix: trigger net.inotify manually on start to ensure instant anti-loopback rule insertion

--- app/src/main/java/com/rox/manager/HomeFragment.java
+++ app/src/main/java/com/rox/manager/HomeFragment.java
@@ -48,12 +48,14 @@ public class HomeFragment extends Fragment {
                 "(pkill -f inotifyd; " +
                 "inotifyd /data/adb/box/scripts/box.inotify /data/adb/modules/box_for_root >/dev/null 2>&1 & " +
                 "inotifyd /data/adb/box/scripts/net.inotify /data/misc/net >/dev/null 2>&1 & " +
-                "inotifyd /data/adb/box/scripts/ctr.inotify /data/misc/net/rt_tables >/dev/null 2>&1 &)", "Starting Box..."));
+                "inotifyd /data/adb/box/scripts/ctr.inotify /data/misc/net/rt_tables >/dev/null 2>&1 & " +
+                "/data/adb/box/scripts/net.inotify w manual)", "Starting Box..."));
         restartBtn.setOnClickListener(v -> runRootAction("/data/adb/box/scripts/box.service restart && " +
                 "(pkill -f inotifyd; " +
                 "inotifyd /data/adb/box/scripts/box.inotify /data/adb/modules/box_for_root >/dev/null 2>&1 & " +
                 "inotifyd /data/adb/box/scripts/net.inotify /data/misc/net >/dev/null 2>&1 & " +
-                "inotifyd /data/adb/box/scripts/ctr.inotify /data/misc/net/rt_tables >/dev/null 2>&1 &)", "Restarting Box..."));
+                "inotifyd /data/adb/box/scripts/ctr.inotify /data/misc/net/rt_tables >/dev/null 2>&1 & " +
+                "/data/adb/box/scripts/net.inotify w manual)", "Restarting Box..."));
         stopBtn.setOnClickListener(v -> runRootAction("/data/adb/box/scripts/box.iptables disable && /data/adb/box/scripts/box.service stop && pkill -f inotifyd", "Stopping Box..."));
 
         return view;
